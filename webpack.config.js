dc983cb18eceba67da26e8081b122e94f73df63e
Herson Salinas <hersonhn@gmail.com>
2018-10-12 01:11:14 -0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,8 @@ module.exports = {
     new UglifyJsPlugin()
   ],
   module: {
-    // loaders: [
-    //   {
-    //     test: /\.js$/,
-    //     loader: 'babel-loader',
-    //     query: {
-    //       presets: ['es2015']
-    //     }
-    //   }
-    // ]
   },
   stats: {
     colors: true
-  },
-  // devtool: 'source-map'
+  }
 };
